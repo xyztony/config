f4b276f6af9d4bb15933f1a53e44206391895aa4
xyztony <awshaw@pm.me>
2024-08-17 09:39:37 +0200

    add vertico-flat ext

--- init.el
+++ init.el
@@ -282,12 +282,19 @@
   (vertico-mode)
   (setq vertico-count 10
 	vertico-resize t
-	vertico-cycle nil)
-  :bind
-  (:map vertico-map
-	("TAB" . vertico-next)
-	("[backtab]" . vertico-previous)))
+	vertico-cycle nil))
 
+(use-package vertico-flat
+  :after vertico
+  :ensure nil
+  :init
+  (vertico-flat-mode)
+  :bind
+  (:map vertico-flat-map
+	("TAB" . vertico-next)
+	([tab] . vertico-next)
+	([backtab] . vertico-previous)
+	("S-TAB" . vertico-previous)))
 
 (use-package vertico-prescient
   :after vertico
@@ -303,7 +310,7 @@
    '("5a00018936fa1df1cd9d54bee02c8a64eafac941453ab48394e2ec2c498b834a" "f00a605fb19cb258ad7e0d99c007f226f24d767d01bf31f3828ce6688cbdeb22" "6128465c3d56c2630732d98a3d1c2438c76a2f296f3c795ebda534d62bb8a0e3" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "74e2ed63173b47d6dc9a82a9a8a6a9048d89760df18bc7033c5f91ff4d083e37" "2ce76d65a813fae8cfee5c207f46f2a256bac69dacbb096051a7a8651aa252b0" "06ed754b259cb54c30c658502f843937ff19f8b53597ac28577ec33bb084fa52" "e266d44fa3b75406394b979a3addc9b7f202348099cfde69e74ee6432f781336" "e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "11cc65061e0a5410d6489af42f1d0f0478dbd181a9660f81a692ddc5f948bf34" "9cd57dd6d61cdf4f6aef3102c4cc2cfc04f5884d4f40b2c90a866c9b6267f2b3" default))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(meow projectile-ripgrep edit-indirect projectile flutter dart-mode exec-path-from-shell markdown-mode evil-org prescient magit vertico-prescient kaolin-themes)))
+   '(marginalia meow projectile-ripgrep edit-indirect projectile flutter dart-mode exec-path-from-shell markdown-mode evil-org prescient magit vertico-prescient kaolin-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
