2509c146b98bd0595a018ef88c906c0d835c6221
Anthony Shaw <anthony.shaw@ardoq.com>
2024-11-16 16:17:30 +0100

    rm projectile

--- lisp/init-nav.el
+++ lisp/init-nav.el
@@ -4,14 +4,6 @@
   (:map dired-mode-map
 	("C-c C-n" . dired-create-empty-file)))
 
-(use-package projectile
-  :init (projectile-mode +1)
-  :config
-  (setq dired-hide-details-hide-information-lines nil)
-  :bind
-  (:map projectile-mode-map
-	("C-c p" . projectile-command-map)))
-
 (use-package wgrep
   :ensure t)
 
