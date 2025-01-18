a1a70850235b51422396dc33f391b35afa0095b7
xyztony <awshaw@pm.me>
2025-01-18 20:26:07 +0100

    avy bindings

--- lisp/init-nav.el
+++ lisp/init-nav.el
@@ -1,3 +1,11 @@
+(use-package avy
+  :bind
+  (:map global-map
+        ("C-c j c" . avy-goto-char)
+        ("C-c j l" . avy-goto-line)
+        ("C-c j m l" . avy-move-line)
+        ("C-c j m r" . avy-move-region)))
+
 (use-package dired
   :after dired
   :bind
