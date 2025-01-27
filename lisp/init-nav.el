a5cd1b2032b801d8ceb099ed2dd2018b6d64eacb
xyztony <awshaw@pm.me>
2025-01-27 21:06:05 +0100

    more mc bindings

--- lisp/init-nav.el
+++ lisp/init-nav.el
@@ -19,12 +19,22 @@
 (use-package wgrep
   :ensure t)
 
+(defun my/mc-mark-word-and-similar ()
+    "Mark word at point and similar words after it."
+    (interactive)
+    (let ((word (thing-at-point 'word t)))
+      (when word
+        (mc/mark-all-words-like-this))))
+
 (use-package multiple-cursors
   :bind
   (:map global-map
         ("C->" . mc/mark-next-like-this)
         ("C-<" . mc/mark-previous-like-this)
-        ("C-c C-<" . mc/mark-all-like-this)))
+        ("C-c C-<" . mc/mark-all-like-this)
+        ("C-C C-C" . mc/edit-lines)
+        ("C-c C-d" . mc/mark-next-like-this)
+        ("C-c C-w" . my/mc-mark-word-and-similar)))
 
 (defun my/beginning-of-line ()
   "Swap between beginning of line, and first char of line."
