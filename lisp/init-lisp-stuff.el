0ce2cd82cbe97ae44c9acb3b5386bc14b5657a9d
xyztony <awshaw@pm.me>
2025-01-31 23:18:51 +0100

    more puni helper

--- lisp/init-lisp-stuff.el
+++ lisp/init-lisp-stuff.el
@@ -33,17 +33,43 @@
         (goto-char end)
         (newline-and-indent)
         (insert sexp-str))))
+
+  (defun my/puni-grab-next-sexp-here ()
+    "Grab the next s-expression and bring it to current position with newline."
+    (interactive)
+    (let ((orig-point (point)))
+      (puni-forward-sexp)
+      
+      (let ((sexp-end (point)))
+        (puni-backward-sexp)
+        (let ((sexp-text (buffer-substring-no-properties (point) sexp-end)))
+          (delete-region (point) sexp-end)
+          (goto-char orig-point)
+          (kill-line)
+          (insert sexp-text)
+          (goto-char orig-point)
+          (newline-and-indent)))))
+
+  (defun my/puni-mark-and-indent-sexp ()
+    "Mark sexp around point using puni and indent the region."
+    (interactive)
+    (let ((orig-point (point)))
+      (puni-mark-sexp-around-point)
+      (indent-region (region-beginning) (region-end))
+      (goto-char orig-point)))
   
   :bind
   (:map puni-mode-map
 	("C-M-s" . puni-mark-sexp-at-point)
-	("C-M-a" . puni-mark-sexp-around-point)
-	("C-M-d" . my/duplicate-region-after)
-
-	("C-M-x" . puni-squeeze)
-	("C-M-z" . puni-splice)
+        ("C-M-a" . puni-mark-sexp-around-point)
         
+        ("C-M-x" . puni-squeeze)
+	("C-M-z" . puni-splice)
+
+        ("C-M-d" . my/duplicate-region-after)
+        ("C-M-e" . my/puni-mark-and-indent-sexp)
         ("C-M-r" . my/puni-raise-and-replace-sexp)
+        ("C-M-g" . my/puni-grab-next-sexp-here)
 	
         ("C-M-b" . puni-slurp-backward)
 	("C-M-f" . puni-slurp-forward)
